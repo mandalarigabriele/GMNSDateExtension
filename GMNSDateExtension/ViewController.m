3b4f43bcdaab1ce3a675f28703a33463b190e1dd
Gabriele Mandalari <mandalari.gabriele@gmail.com>
2014-10-01 23:23:23 +0200

    First Commit

--- GMNSDateExtension/ViewController.m
+++ GMNSDateExtension/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "GMNSDateExtension.h"
 
 @interface ViewController ()
 
@@ -15,13 +16,46 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
+	
 	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
+	
+	BOOL isToday = [GMNSDateExtension isToday: [NSDate date] withFormat:[GMNSDateExtension dateFormat:DateFormatAmerican]];
+	
+	NSLog(isToday ? @"isToday = Yes" : @"isToday = No");
+	
+	NSDate *tomorrow = [GMNSDateExtension dateTomorrowWithFormat: [GMNSDateExtension dateFormat: DateFormatEuropean]];
+	
+	NSDate *yesterday = [GMNSDateExtension dateYesterdayWithFormat: [GMNSDateExtension dateFormat: DateFormatEuropean]];
+	
+	BOOL isTomorrow = [GMNSDateExtension isTomorrow: tomorrow withFormat:[GMNSDateExtension dateFormat:DateFormatEuropean]];
+	
+	NSLog(isTomorrow ? @"isTomorrow = Yes" : @"isTomorrow = No");
+	
+	BOOL isYesterday = [GMNSDateExtension isYesterday: yesterday withFormat:[GMNSDateExtension dateFormat:DateFormatEuropean]];
+	
+	NSLog(isYesterday ? @"isYesterday = Yes" : @"Yesterday = No");
+	
+	NSInteger daysDifference = [GMNSDateExtension daysDifferenceFromDate:yesterday toDate:tomorrow withFormat:[GMNSDateExtension dateFormat: DateFormatEuropean]];
+	
+	NSLog(@"Days Difference %ld", (long)daysDifference);
+	
+	NSInteger yearsDifference = [GMNSDateExtension yearsDifferenceFromDate: [NSDate dateWithTimeIntervalSince1970:0] toDate:tomorrow withFormat:[GMNSDateExtension dateFormat: DateFormatEuropean]];
+	
+	NSLog(@"Years Difference %ld", (long)yearsDifference);
+	
+	NSInteger monthsDifference = [GMNSDateExtension monthsDifferenceFromDate: [NSDate dateWithTimeIntervalSince1970:0] toDate:tomorrow withFormat:[GMNSDateExtension dateFormat: DateFormatEuropean]];
+	
+	NSLog(@"Month Difference %ld", (long)monthsDifference);
+	
+	NSInteger secondsDifference = [GMNSDateExtension secondsDifferenceFromDate: [NSDate dateWithTimeIntervalSince1970:0] toDate:tomorrow withFormat:[GMNSDateExtension dateFormat: DateFormatEuropean]];
+	
+	NSLog(@"Seconds Difference %ld", (long)secondsDifference);
+	
+	NSInteger dayComponent = [GMNSDateExtension dayComponenetFromDate:[NSDate date] withFormat:@"dd-MM-yyyy"];
+	
+	NSLog(@"Day Component %ld", (long)dayComponent);
 
-- (void)didReceiveMemoryWarning {
-	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
+	
 }
 
 @end
