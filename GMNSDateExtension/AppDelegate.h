b4bb6093c11ffe9a217a7294926bbf953db68b4c
Gabriele Mandalari <mandalari.gabriele@gmail.com>
2014-10-01 18:20:17 +0200

    Initial Commit

---
+++ GMNSDateExtension/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GMNSDateExtension
+//
+//  Created by Gabriele Mandalari on 01/10/14.
+//  Copyright (c) 2014 Gabriele Mandalari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GMNSDateExtension/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GMNSDateExtension
+//
+//  Created by Gabriele Mandalari on 01/10/14.
+//  Copyright (c) 2014 Gabriele Mandalari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
